a2f73ec9ca1c9f97b282bd4ecdede0436b0e2367
Griffenck1 <griffenck1@gmail.com>
2021-02-21 21:04:11 -0700

    added file structure and index page

---
+++ server.js
@@ -0,0 +1,28 @@
+//dependancies for node.js server
+var express = require('express');
+var app = express();
+var url = require('url');
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+const bodyParser = require("body-parser");
+const { response } = require('express');
+const router = express.Router();
+
+//allows the app to use .ejs files with a view engine
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));
+
+/*
+Index/ Landing Page
+*/
+app.get('/', function(req, res) {
+    res.render('./pages/index',{
+        my_title: "index",
+        data: ``
+    })
+});
+
+app.listen(8082);
